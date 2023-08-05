91e0702158c63b934eea8a8ad9d5e4335a3a39f8
Akash Kumar <akashkumar59999@gmail.com>
2023-08-05 22:22:36 +0530

    done with UI

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import classes from "./Header.module.css";
+import mealsImage from "../../assets/back.jpg";
+
+import HeaderCardButton from "./HeaderCardButton";
+
+const Header = () => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>AkashMeals</h1>
+        <HeaderCardButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="a table full of delicious food" />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
